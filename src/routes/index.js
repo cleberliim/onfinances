7927e4ffebec342e10bd1a42381d191265137c14
cleberliim <cleberliim@outlook.com>
2024-06-11 16:38:26 -0300

    deleted:    src/controllers/revenueController.js  	deleted:    src/models/Revenue.js  	deleted:    src/repositories/revenueRepository.js  	modified:   src/routes/index.js  	deleted:    src/routes/revenueRoutes.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const entryRoutes = require("./entryRoutes");
-const revenueRoutes = require("./revenueRoutes");
 const expenseRoutes = require("./expenseRoutes");
 
 const router = express.Router();
 
 router.use("/entries", entryRoutes);
-router.use("/revenues", revenueRoutes);
 router.use("/expenses", expenseRoutes);
 
 module.exports = router;
